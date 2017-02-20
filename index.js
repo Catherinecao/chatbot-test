7da87418e89a11fa0b2084e851ec627643a66caa
Catherine Cao <catherinecao25@gmail.com>
2017-02-21 11:49:00 +1300

    test

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var request = request('request');
-var app = express;
+var express = require('express')
+var bodyParser = require('body-parser')
+var request = request('request')
+var app = express
 
-app.set('post',(process.env.PORT || 5000));
+app.set('post',(process.env.PORT || 5000))
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({extended:false}))
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 app.get('/', function(req,res){
-	res.send('Hello world. I am a chat bot');
-});
+	res.send('Hello world. I am a chat bot')
+})
 
 app.get('/webhook', function(req, res) {
   if (req.query['hub.verify_token'] === 'My_Password') {
-    console.log("Validating webhook");
-    res.status(200).send(req.query['hub.challenge']);
+    console.log("Validating webhook")
+    res.status(200).send(req.query['hub.challenge'])
   } else {
-    console.error("Failed validation. Make sure the validation tokens match.");
-    res.sendStatus(403);          
-  }  
-});
+    console.error("Failed validation. Make sure the validation tokens match.")
+    res.sendStatus(403)
+  }
+})
 
 app.listen(app.get('port'),function(){
-  console.log('running on port',app.get('port'));
-});
-
+  console.log('running on port',app.get('port'))
+})
