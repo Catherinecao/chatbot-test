ef7aafbc38231ae2b84cf4b84e386852fed0d224
Catherine Cao <catherinecao25@gmail.com>
2017-02-21 12:34:16 +1300

    securing webhook

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var bodyParser = require('body-parser')
 var request = require('request')
 var app = express()
 
+var token = process.env.FB_VERIFY_TOKEN
+var access = process.env.FB_ACCESS_TOKEN
+
 app.set('port',(process.env.PORT || 5000))
 
 app.use(bodyParser.urlencoded({extended:false}))
@@ -14,7 +17,7 @@ app.get('/', function(req,res){
 })
 
 app.get('/webhook', function (req, res) {
-    if (req.query['hub.verify_token'] === 'YOUR_VERIFY_TOKEN') {
+    if (req.query['hub.verify_token'] === token) {
       res.send(req.query['hub.challenge'])
     } else {
       res.send('Error, wrong validation token')
