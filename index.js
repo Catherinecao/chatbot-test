de0f18bfe79bc3a93cc4976d1ba1bc1313eb0ad1
Catherine Cao <catherinecao25@gmail.com>
2017-02-21 12:04:28 +1300

    test

--- index.js
+++ index.js
@@ -16,10 +16,9 @@ app.get('/', function(req,res){
 app.get('/webhook', function(req, res) {
   if (req.query['hub.verify_token'] === 'My_Password') {
     console.log("Validating webhook")
-    res.status(200).send(req.query['hub.challenge'])
+    res.send(req.query['hub.challenge'])
   } else {
     console.error("Failed validation. Make sure the validation tokens match.")
-    res.sendStatus(403)
   }
 })
 
